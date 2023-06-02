89790a87857ec49a6c327e447c3f1c1285b32880
welinton <wesgdeveloper@gmail.com>
2023-06-02 10:32:04 -0500

    feat: se acopla proyecto de codepain

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CharacterCard from "./components/CharacterCard";
+
+import data from "./assets/data.json";
+
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Rick and Morty Characters</h1>
+      <h2>Your code here</h2>
+
+      <div className="container">
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+const CharacterCard = (props) => (
+  <article className="characterCard">
+    <div className="characterImagen">
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Blue Footprint Guy"
+      />
+    </div>
+    <div className="characterContent">
+      <div className="section">
+        <h2>Morty Smith</h2>
+        <span className="status">
+          <span className={`status__icon status__alive`}></span>
+          Alive - Human
+        </span>
+      </div>
+      <div className="section">
+        <span className="text-gray">Last known location:</span>
+        <a href="https://rickandmortyapi.com/api/location/1">Earth</a>
+      </div>
+      <div className="section">
+        <span className="text-gray">First seen in:</span>
+        <a href="https://rickandmortyapi.com/api/location/20">Earth</a>
+      </div>
+    </div>
+  </article>
+);
+
+export default CharacterCard;
