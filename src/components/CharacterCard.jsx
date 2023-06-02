fab7c6bc9ac83b04eed3247abf0f761ec4439672
welinton <wesgdeveloper@gmail.com>
2023-06-02 13:55:38 -0500

    feat: se usan props en componente CharacterCard

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,26 +3,29 @@ import "./styles.css";
 const CharacterCard = ({ character }) => (
   <article className="characterCard">
     <div className="characterImagen">
-      <img
-        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-        alt="Blue Footprint Guy"
-      />
+      <img src={character.image} alt="Blue Footprint Guy" />
     </div>
     <div className="characterContent">
       <div className="section">
         <h2>{character.name}</h2>
         <span className="status">
-          <span className={`status__icon status__alive`}></span>
-          Alive - Human
+          <span
+            className={
+              character.status === "Alive"
+                ? `status__icon status__alive`
+                : `status__icon status__dead`
+            }
+          ></span>
+          {`${character.status} - ${character.species}`}
         </span>
       </div>
       <div className="section">
         <span className="text-gray">Last known location:</span>
-        <a href="https://rickandmortyapi.com/api/location/1">Earth</a>
+        <a href={character.location.url}>{character.location.name}</a>
       </div>
       <div className="section">
         <span className="text-gray">First seen in:</span>
-        <a href="https://rickandmortyapi.com/api/location/20">Earth</a>
+        <a href={character.origin.url}>{character.origin.name}</a>
       </div>
     </div>
   </article>
